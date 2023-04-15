8f853bc03ba915120c0d8f90315a17e8390d614b
antonkalmbah <anton.kalmbah@yandex.ru>
2023-04-15 11:44:07 +0300

    sum_array.py work_2

--- sum_array.py
+++ sum_array.py
@@ -1,11 +1,11 @@
 a = [1, 5.2, 4, 0, -1]
-sum = 0
 
 
-def sum_array(a, sum = 0):
+def sum_array(a):
+    summary = 0
     for i in a:
-        sum += i
-    return sum
+        summary += i
+    print(summary)
 
 
 sum_array(a)
